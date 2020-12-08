95128ffd40c1d4c0748de1835c6cd80fd1e32d5c
Sheer Maoz <sheermaoz@gmail.com>
2020-12-08 15:31:54 +0200

    Implemented attackEvent

--- src/main/java/bgu/spl/mics/application/messages/AttackEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/AttackEvent.java
@@ -1,6 +1,25 @@
 package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 
+import java.util.LinkedList;
+import java.util.List;
+import bgu.spl.mics.application.passiveObjects.*;
+
 public class AttackEvent implements Event<Boolean> {
-	
+    
+    final int time;
+    final List<Integer> serials;
+
+    public AttackEvent(Attack attack)
+    {
+        time = attack.getDuration();
+        serials = attack.getSerials();
+    }
+
+    public AttackEvent()
+    {
+        time = 0;
+        serials = new LinkedList<>();
+    }
+
 }
