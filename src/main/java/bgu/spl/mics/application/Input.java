fe5a1fbcd7f217120f1a89df726c47c6bbebe5ae
Sheer Maoz <sheermaoz@gmail.com>
2020-12-13 19:16:56 +0200

    Added javadoc

--- src/main/java/bgu/spl/mics/application/Input.java
+++ src/main/java/bgu/spl/mics/application/Input.java
@@ -8,27 +8,66 @@ public class Input {
 	private int Lando;
 	private int Ewoks;
 	
+	/**
+	 * Gets the number of ewoks available.
+	 * @return number of ewoks.
+	 */
 	public int getEwoks() {
 		return Ewoks;
 	}
+
+	/**
+	 * Sets number of available ewoks to ewoks
+	 * @param ewoks number of ewoks
+	 */
 	public void setEwoks(int ewoks) {
 		Ewoks = ewoks;
 	}
+	
+	/**
+	 * Gets Lando's action time.
+	 * @return Lando's time.
+	 */
 	public int getLando() {
 		return Lando;
 	}
+
+	/**
+	 * Sets Lando's action time to lando.
+	 * @param lando Lando's action time.
+	 */
 	public void setLando(int lando) {
 		Lando = lando;
 	}
+
+	/**
+	 * Gets R2D2's action time.
+	 * @return R2D2's time.
+	 */
 	public int getR2D2() {
 		return R2D2;
 	}
+
+	/**
+	 * Sets R2D2's action time to r2d2.
+	 * @param r2d2 R2D2's action time.
+	 */
 	public void setR2D2(int r2d2) {
 		R2D2 = r2d2;
 	}
+
+	/**
+	 * Gets an array of Attacks to use for Leia.
+	 * @return an array of Attacks.
+	 */
 	public Attack[] getAttacks() {
 		return attacks;
 	}
+
+	/**
+	 * Sets Leia's Attack array to attacks.
+	 * @param attacks the array to use for Leia's attacks.
+	 */
 	public void setAttacks(Attack[] attacks) {
 		this.attacks = attacks;
 	}
--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -28,97 +28,171 @@ public class Diary {
     {
         totalAttacks = new AtomicInteger();
     }
-    
+    /**
+     * This function return the instance of Diary.
+     * @return instance.
+     */
     public static Diary getInstance()
     {
         return DiaryHolder.instance;
     }
 
+    /**
+     * This function return the number of total attacks.
+     * @return total attacks.
+     */
     public AtomicInteger getTotalAttack()
     {
         return totalAttacks;
     }
     
+    /**
+     * This function return the time in which HanSolo finished his attacks.
+     * @return time in milliseconds.
+     */
     public long getHanSoloFinish()
     {
         return HanSoloFinish;
     }
 
+    /**
+     * This function return the time in which C3PO finished his attacks.
+     * @return time in milliseconds.
+     */
     public long getC3POFinish()
     {
         return C3POFinish;
     }
 
+    /**
+     * This function return the time in which R2D2 finished his deactivation.
+     * @return time in milliseconds.
+     */
     public long getR2D2Deactivate()
     {
         return R2D2Deactivate;
     }
 
+    /**
+     * This function return the time in which Leia finished runnning.
+     * @return time in milliseconds.
+     */
     public long getLeiaTerminate()
     {
         return LeiaTerminate;
     }
 
+    /**
+     * This function return the time in which HanSolo finished runnning.
+     * @return time in milliseconds.
+     */
     public long getHanSoloTerminate()
     {
         return HanSoloTerminate;
     }
 
+    /**
+     * This function return the time in which C3PO finished runnning.
+     * @return time in milliseconds.
+     */
     public long getC3POTerminate()
     {
         return C3POTerminate;
     }
 
+    /**
+     * This function return the time in which R2D2 finished runnning.
+     * @return time in milliseconds.
+     */
     public long getR2D2Terminate()
     {
         return R2D2Terminate;
     }
 
+    /**
+     * This function return the time in which Lando finished runnning.
+     * @return time in milliseconds.
+     */
     public long getLandoTerminate()
     {
         return LandoTerminate;
     }
         
+    /**
+     * Increases the number of attacks completed.
+     */
     public void addAttack()
     {
         totalAttacks.incrementAndGet();
     }
 
+    /**
+     * Sets HanSolo's attack finish time as time.
+     * @param time: time in which HanSolo finished.
+     */
     public void setHanSoloFinish(long time)
     {
         HanSoloFinish = time;
     }
 
+    /**
+     * Sets C3PO's attack finish time as time.
+     * @param time: time in which C3PO finished.
+     */
     public void setC3POFinish(long time)
     {
         C3POFinish = time;
     }
 
+    /**
+     * Sets R2D2's deactiovation finish time as time.
+     * @param time: time in which R2D2 finished.
+     */
     public void setR2D2Deactivate(long time)
     {
         R2D2Deactivate = time;
     }
 
+    /**
+     * Sets Leia's termination time as time.
+     * @param time: time in which Leia terminated.
+     */
     public void setLeiaTerminate(long time)
     {
         LeiaTerminate = time;
     }
 
+    /**
+     * Sets HanSolo's termination time as time.
+     * @param time: time in which HanSolo terminated.
+     */
     public void setHanSoloTerminate(long time)
     {
         HanSoloTerminate = time;
     }
 
+    /**
+     * Sets C3PO's termination time as time.
+     * @param time: time in which C3PO terminated.
+     */
     public void setC3POTerminate(long time)
     {
         C3POTerminate = time;
     }
 
+    /**
+     * Sets R2D2's termination time as time.
+     * @param time: time in which R2D2 terminated.
+     */
     public void setR2D2Terminate(long time)
     {
         R2D2Terminate = time;
     }
 
+    /**
+     * Sets Lando's termination time as time.
+     * @param time: time in which Lando terminated.
+     */
     public void setLandoTerminate(long time)
     {
         LandoTerminate = time;
